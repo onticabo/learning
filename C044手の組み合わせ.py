f7743590be7275d971b788404a9b1ee5a10545fa
tsubone72 <tsubone@gmail.com>
2020-05-29 23:24:29 +0900

    C44

---
+++ C044手の組み合わせ.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+Number = int(input())
+
+hands_count = { 'paper':0, 'rock':0, 'scissors':0 }
+win_or_lose = { 'paper':'rock', 'rock':'scissors', 'scissors':'paper'}
+
+for i in range(Number):
+    hands_count[input()] += 1
+
+remain_count = 0
+remain_hand = ''
+
+for key, value in hands_count.items():
+    if value == 0:
+        remain_hand = key
+        remain_count += 1
+
+if remain_count == 1:
+    print(win_or_lose[remain_hand])
+else:
+    print('draw')
