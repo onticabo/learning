f964880fb474b1f57a516b09717137ec4a2897af
tsubone72 <64714337+tsubone72@users.noreply.github.com>
2020-07-01 14:13:00 +0900

    Update B065:小さなライフゲーム.py

--- B065:小さなライフゲーム.py
+++ B065:小さなライフゲーム.py
@@ -1,24 +1,24 @@
 # coding: utf-8
 # from my_input import input
 
-def new_life(line, rule):
-    next_dic = dict(zip(['--', '-+', '+-', '++'], rule))
-    expand = line[-1] + line + line[0]
+def new_life(line, next):
+    expand = line[-1] + line + line[0] # 端を拡張
     new_life = ''
     for n in range(1,len(expand)-1):
-        key = expand[n-1] + expand[n+1]
-        new_life += next_dic[key]
+        key = expand[n-1] + expand[n+1] # 左と右を足してキーにする
+        new_life += next[key]   # 新しい文字を取得
     return new_life
 
 initial, rule = input().split()
-
 life_status = initial
-history = []
-while initial not in history:
-    life_status = new_life(life_status, rule)
-    if life_status in history:
-        print("NO")
+next_dic = dict(zip(['--', '-+', '+-', '++'], rule))
+
+history = []                    # 履歴を保存するリスト
+while initial not in history:   # 初期配列に戻らないうちは次を調べる
+    life_status = new_life(life_status, next_dic)
+    if life_status in history:  # 初期配置ではないのに履歴にはあるということは
+        print("NO")             # 永久に初期配置には戻らない
         break
-    history.append(life_status)
+    history.append(life_status) # 履歴を保存
 else:
-    print("YES")
+    print("YES")                # 初期配置に戻ったのでYES
